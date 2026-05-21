773ffa851f4d95d5df5f062faf79d4c75df6e97c
camilazamenfeld <camizamenfeld@gmail.com>
2026-05-21 12:02:56 -0300

    .

---
+++ Assets/Scripts/InteractionArea.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionArea : MonoBehaviour
+{
+    public int score = 0;
+
+    public UIManager UIManagerScript;
+
+    void Awake()
+    {
+        UIManagerScript = GameObject.FindObjectOfType<UIManager>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Collectible"))
+        {
+            Destroy(other.gameObject);
+
+            score++;
+
+            UIManagerScript.UpdateScore(score);
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI txtScore;
+    public TextMeshProUGUI txtTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScore(0);
+    }
+public void UpdateScore(int score)
+{
+   txtScore.text = "Score: " + score.ToString();
+}
+ 
+}
